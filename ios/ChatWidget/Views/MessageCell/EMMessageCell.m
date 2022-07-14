fcc4a06c28f12b2ca3e2fac795671b7ec74a62fb
sheen <tamworth@163.com>
2022-07-14 16:01:47 +0800

    update localized

--- ios/ChatWidget/Views/MessageCell/EMMessageCell.m
+++ ios/ChatWidget/Views/MessageCell/EMMessageCell.m
@@ -11,6 +11,7 @@
 #import "EMMsgTextBubbleView.h"
 #import "EMMsgImageBubbleView.h"
 #import <SDWebImage/UIImageView+WebCache.h>
+#import "AGResourceManager.h"
 @import AgoraUIBaseViews;
 
 @interface EMMessageCell()
@@ -112,7 +113,7 @@
     _roleTag.leftViewMode = UITextFieldViewModeAlways;
     _roleTag.rightView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 8, 0)];
     _roleTag.rightViewMode = UITextFieldViewModeAlways;
-    _roleTag.text = [@"fcr_hyphenate_im_teacher" ag_localizedIn:@"AgoraWidgets"];
+    _roleTag.text = [@"fcr_hyphenate_im_teacher" ag_localized];
     _roleTag.enabled = NO;
     [self.contentView addSubview:_roleTag];
     
@@ -262,7 +263,9 @@
         }
     }
     if(role.longValue == 1 || role.longValue == 3) {
-        self.roleTag.text = role.longValue == 1?[@"fcr_hyphenate_im_teacher" ag_localizedIn:@"AgoraWidgets"] : [@"fcr_hyphenate_im_assistant" ag_localizedIn:@"AgoraWidgets"];
+        self.roleTag.text = role.longValue == 1 ?
+        [@"fcr_hyphenate_im_teacher" ag_localized] :
+        [@"fcr_hyphenate_im_assistant" ag_localized];
         self.roleTag.hidden = NO;
     }else{
         self.roleTag.hidden = YES;
--- ios/RNAgoraRteChatview.podspec
+++ ios/RNAgoraRteChatview.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name         = "RNAgoraRteChatview"
-  s.version      = "1.1.1"
+  s.version      = "1.1.2"
   s.summary      = "RNAgoraRteChatview"
   s.description  = <<-DESC
                   RNAgoraRteChatview
